a41cc6883b1646a9715aeb2fe7a58dd05e234e19
Piyali Banerjee <pbanerjee@pivotal.io>
2019-11-19 09:45:55 -0800

    specify name when helm installing
    Authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -5,9 +5,9 @@ set -ex
 SCRIPT_DIR=$(dirname $0)
 
 #database
-helm install --name capi-database stable/postgresql -f "${SCRIPT_DIR}/postgresql-values.yaml"
+helm install capi-database stable/postgresql -f "${SCRIPT_DIR}/postgresql-values.yaml"
 #minio
-helm install --name capi-blobstore stable/minio
+helm install capi-blobstore stable/minio
 
 #capi
 helm template "${SCRIPT_DIR}/.." --set-string system_domain=minikube.local -f "${SCRIPT_DIR}/capi-values.yaml" | kubectl apply -f -
