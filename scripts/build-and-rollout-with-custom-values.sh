89a7859dafba50afae12d6fa74078ee37515365d
Piyali Banerjee <pbanerjee@pivotal.io>
2020-01-27 10:14:28 -0800

    wip ccdb ytt config

---
+++ scripts/build-and-rollout-with-custom-values.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+SCRIPT_DIR=$(dirname $0)
+REPO_BASE_DIR="${SCRIPT_DIR}/.."
+
+${SCRIPT_DIR}/build.sh
+
+docker push $(minikube ip):5000/capi
+
+${SCRIPT_DIR}/rollout-with-custom-values.sh
