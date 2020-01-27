779377581f115f534b0daa56ad0b68f72ab28b81
Piyali Banerjee <pbanerjee@pivotal.io>
2020-01-27 10:14:28 -0800

    wip ccdb ytt config

--- scripts/build-and-rollout.sh
+++ scripts/build-and-rollout.sh
@@ -4,9 +4,16 @@ set -ex
 
 SCRIPT_DIR=$(dirname $0)
 REPO_BASE_DIR="${SCRIPT_DIR}/.."
-
+eval $(minikube docker-env)
 ${SCRIPT_DIR}/build.sh
 
 docker push $(minikube ip):5000/capi
 
-${SCRIPT_DIR}/rollout.sh
+if [ -n "$1" ]
+then
+	${SCRIPT_DIR}/rollout.sh $1 
+else
+	${SCRIPT_DIR}/rollout.sh
+fi
+
+
